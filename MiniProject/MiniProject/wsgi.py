481a9eac620a9e00c2e58d24c0b008b157e4a3c1
valanirmal18 <nirmalvala18@gmail.com>
2025-04-17 10:07:33 +0530

    first commit

--- MiniProject/MiniProject/wsgi.py
+++ MiniProject/MiniProject/wsgi.py
@@ -1,18 +1,12 @@
-"""
-WSGI config for MiniProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+from pathlib import Path
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the Python path
+project_dir = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(project_dir))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MiniProject.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-app = application
